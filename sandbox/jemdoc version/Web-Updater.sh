df0f114328b7bf6b23fc0502d84ef4a4fc57d8a7
Rod Kennedy <rodneykennedy@Rodneys-MacBook.gateway>
2016-09-07 11:27:45 +1000

    sand4

--- sandbox/jemdoc version/Web-Updater.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "web script started"
-cd ../../..
-pwd
-jemdoc-rak -o html/ -c mysite.conf index book errata links masters phd pubs rakdoc students trivia
-sleep 2
-open "html/index.html"
--- sandbox/jemdoc version/sandbox/web-generate-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/tcsh
-echo "...executing script"
-jemdoc-rak -o html/ -c mysite.conf index book errata links masters phd pubs students trivia
-sleep 1
-open "html/index.html"
