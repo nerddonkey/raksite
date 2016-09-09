2da7f05d2aa0a299466d84134e3c5d37fd2096c7
Rod Kennedy <rodk@Captain-Haddock.local>
2016-09-09 15:17:39 +1000

    Fixes

--- anu-deploy.sh
+++ anu-deploy.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
-echo -e "\nraksite anu-deploy"
+echo -e "\nRunning $0\n"
 pwd
 which -a jekyll
 jekyll -v
 echo -e "\n"
 
-# deploy on base url: http://users.cecs.anu.edu.au/~rod
+echo -e "Check you have mounted 'rod@otho.cecs.anu.edu.au'\n"
+
 jekyll build --config _config.yml,_config-anu.yml
 cd _site
 ls -l *.html
 
 cp -rv * /Volumes/rod/public_html
+cd ..
 
 open "http://users.cecs.anu.edu.au/~rod/"
--- anu-serve.sh
+++ anu-serve.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-echo -e "\nraksite anu-serve\n"
+echo -e "\nRunning $0\n"
 pwd
 which -a jekyll
 jekyll -v
@@ -7,5 +7,4 @@ echo -e "\n"
 
 (sleep 6; open "http://127.0.0.1:4003/" ) &
 
-# deploy on base url: ""
 jekyll serve
